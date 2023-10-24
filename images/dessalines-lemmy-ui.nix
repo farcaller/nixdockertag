fdc641dfb30a1a2bf4ac330758f78cc2d0a85f05
Vladimir Pouzanov <farcaller@gmail.com>
2023-10-24 18:51:55 +0000

    Remove lemmy images: no more floating tags

--- images/dessalines-lemmy-ui.nix
+++
@@ -1,5 +0,0 @@
-{
-  image = "registry-1.docker.io/dessalines/lemmy-ui";
-  followTag = "latest";
-  hash = "cdfa3743260306ad1ddb67c32188cb2953ed58e13ef5f5c6776e72ab2f0954b8";
-}
--- images/dessalines-lemmy.nix
+++
@@ -1,5 +0,0 @@
-{
-  image = "registry-1.docker.io/dessalines/lemmy";
-  followTag = "latest";
-  hash = "9ee23a327ba9074afb12d6b210637c81669f94fd40f3e1fd5f5fd3c55774f67d";
-}

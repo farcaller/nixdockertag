fa6edcca9b95810b5ae2c8921dc2fcc3c29ee0b2
Vladimir Pouzanov <farcaller@gmail.com>
2024-08-27 18:50:42 +0000

    postgres: update to 492898505cb45f9835acc327e98711eaa9298ed804e0bb36f29e08394229550d

--- images/postgres-16-alpine.nix
+++ images/postgres-16-alpine.nix
@@ -1,5 +1,5 @@
 {
-  image = "registry-1.docker.io/postgres";
+  image = "postgres";
   followTag = "16-alpine";
-  hash = "";
+  hash = "492898505cb45f9835acc327e98711eaa9298ed804e0bb36f29e08394229550d";
 }
